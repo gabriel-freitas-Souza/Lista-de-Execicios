f497a1aca55e81c5cfb2105c8e376ea55cd30074
gabriel-freitas-Souza <freitassouza26@gmail.com>
2021-03-27 11:02:31 -0300

    Update exercicio_4.c

--- faculdade/exercicio_4.c
+++ faculdade/exercicio_4.c
@@ -1,42 +1,51 @@
 /*
- * 
+ *
  * ===  ===  === Exercício  4 ===  ===  ===
  * FACA UM PROGRAMA QUE RECEBA TRES NUMEROS
  * REAIS DISTINTOS E IMPRIMA-OS EM ORDEM
  * CRESCENTE.
  * === === === === === === === === === ===
  *
+ * === === ===  LOGICA DO PROGRAMA === === === ===
+ *   O PROGRAMA PEDIR TRES NUMEROS AO USUARIO E
+ *  DEPOIS COMPARAR PARA VER QUAL É MENOR E O
+ *  SEGUNDO MENOR.
+ *   E RETORNAR PARA O USUARIO A RESPOSTA
+ *  MOSTRANDO EM ORDEM CRESCENTE.
+ * === === === === === === === === === === === ===
  */
 #include <stdio.h>
 
 int main() {
     //VARIAVEIS
-    float number[4]; // CRIAR UM VETOR DE 4 POSICOES PARA ARMAZENAR OS 3 NUMEROS DIGITADOS
-    // LAÇO DE REPEDIÇÃO/ENTRADA
-    for (int i = 1; i <= 3; i++) { // PEDIR OS 3 NUMEROS AO USUÁRIO
-        printf("DIGITE O NUMERO %i : \n--> ", i); // PEDIR PARA O NUMEOR AO USUÁRIO
-        scanf("%f", &number[i]); // LER E GUARDA DENTRO DO VETOR
+    float numero[3]; // Criar um vetor de 3 posicicoes para armazenar 3 numeros digitados.
+    // LAÇO DE REPEDIÇÃO && ENTRADA
+    for (int i = 0; i < 3; i++) { // Pedir ao usuario os 3 numeros.
+        printf("DIGITE O NUMERO %i : \n--> ", i+1); // Pedir ao usuario o numero(imprimi).
+        scanf("%f", &numero[i]); // Ler e guarda dentro od vetor.
     }
     //LAÇO CONDICIONAL 
-    if (number[1] > number[2] && number[1] > number[3]) {// VERIFICAR SE PRIMEIRO NUMERO DIGITADO E MAIOR
-        if (number[2] > number[3]){ //VERIFICAR O SEGUNDO  MAIOR NUMERO
-            printf("%.2f , %.2f , %.2f", number[1], number[2], number[3]); // RESULTADO 1
+    if (numero[0] < numero[1] && numero[0] < numero[2]) {// Verificar se primeiro numero digitado e menor
+        if (numero[1] < numero[2]){ // Verificar o segundo  menor numero
+            printf("%f , %f , %f", numero[0], numero[1], numero[2]); // Resultado 1
         } else {
-            printf("%.2f , %.2f , %.2f", number[1], number[3], number[2]);// RESULTADO 2
+            printf("%f , %f , %f", numero[0], numero[2], numero[1]);// Resultado 2
         }
-    } else if (number[2] > number[1] && number[2] > number[3]) {// VERIFICAR SE SEGUNDO NUMERO DIGITADO E MAIOR
-        if (number[1] > number[3]){//VERIFICAR O SEGUNDO  MAIOR NUMERO
-            printf("%.2f , %.2f , %.2f", number[2], number[1], number[3]); // RESULTADO 3
+    } else if (numero[1] < numero[0] && numero[1] < numero[2]) {// Verificar se segundo numero digitado \\
+                                                                   e menor
+        if (numero[0] < numero[2]){// Verificar o segundo  menor numero
+            printf("%f , %f , %f", numero[1], numero[0], numero[2]); // Resultado 3
         } else {
-            printf("%.2f , %.2f , %.2f", number[2], number[3], number[1]); // RESULTADO 4
+            printf("%f , %f , %f", numero[1], numero[2], numero[0]); // Resultado 4
         }
-    }else if (number[3] > number[1] && number[3] > number[2]) {// VERIFICAR SE TERCEIRO NUMERO DIGITADO E MAIOR
-        if (number[1] > number[2]) {//VERIFICAR O SEGUNDO  MAIOR NUMERO
-            printf("%.2f , %.2f , %.2f", number[3], number[1], number[2]); // RESULTADO 5
-        } else{
-            printf("%.2f , %.2f , %.2f", number[3], number[2], number[1]); // RESULTADO 6
+    }else if (numero[2] < numero[0] && numero[2] < numero[1]) {// Verificar se terceiro numero digitado \\
+                                                                  e menor
+        if (numero[0] < numero[1]) {// Verificar o segundo  menor numero
+            printf("%f , %f , %f", numero[2], numero[0], numero[1]); // Resultado 5
+        } else {
+            printf("%f , %f , %f", numero[2], numero[1], numero[0]); // Resultado 6
         }
     }
     //SAIDA
     return 0;
-}
+}// 'Copr.Gabriel_Freitas_Souza'
